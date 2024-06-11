b688000e54ea191629166826b40dd873ab8b5972
Сергей Аветисов <karolina.kovdya-onikienko@yandex.ru>
2024-06-11 10:09:42 +0300

    add style for navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,7 +4,32 @@ import { Link } from 'react-router-dom';
 function Navigation () {
     return (
         <section className='navigation'>
-
+          <ul className='navigation__list'>
+            <li className="navigation__links">
+              <Link to="/" className="navigation__link" >Вкладка1</Link>
+            </li>
+            <li className="navigation__links">
+              <Link to="/" className="navigation__link" >Вкладка2</Link>
+            </li>
+            <li className="navigation__links">
+              <Link to="/" className="navigation__link" >Вкладка3</Link>
+            </li>
+            <li className="navigation__links">
+              <Link to="/" className="navigation__link" >Вкладка4</Link>
+            </li>
+            <li className="navigation__links">
+              <Link to="/" className="navigation__link" >Вкладка5</Link>
+            </li>
+            <li className="navigation__links">
+              <Link to="/" className="navigation__link" >Вкладка6</Link>
+            </li>
+            <li className="navigation__links">
+              <Link to="/" className="navigation__link" >Вкладка7</Link>
+            </li>
+            <li className="navigation__links">
+              <Link to="/" className="navigation__link" >Вкладка8</Link>
+            </li>
+          </ul>
         </section>
     )
 }
