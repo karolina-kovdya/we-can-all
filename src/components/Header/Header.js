320cd135b414aff1ba78d1359038df26b7f244f3
Сергей Аветисов <karolina.kovdya-onikienko@yandex.ru>
2024-06-11 08:12:03 +0300

    refactor header, change style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import './Header.css';
+import icon from '../../images/logo_telegram_airplane_air_plane_paper_airplane_icon_143169.png'
 
 function Header () {
     return (
@@ -6,6 +7,17 @@ function Header () {
             <div className='header__info'>
               <p className='header__info-text'>Сайт создан при поддержке сообщества</p>
               <p className='header__info-name'>«Мы можем все»</p>
+              <a 
+                className='header__link' 
+                href='https://t.me/+zpdVmwyIunhmOGMy'
+                target='blanc'
+                >
+                <img 
+                  className='header__icon' 
+                  alt='иконка телеграм' 
+                  src={icon}
+                />
+              </a>
             </div>
             <div className='header__logo'>LO<br></br>GO</div>
         </header>
