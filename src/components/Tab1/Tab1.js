0e5be81ad2f7e383713cef7f97f105fa5cf07391
Сергей Аветисов <karolina.kovdya-onikienko@yandex.ru>
2024-06-18 20:26:31 +0300

    add ul to Tab1

--- src/components/Tab1/Tab1.js
+++ src/components/Tab1/Tab1.js
@@ -5,57 +5,69 @@ import file1 from '../../files/ClinikC21.pdf';
 import file2 from '../../files/PrC21.pdf';
 import file3 from '../../files/RC21.pdf';
 
-function Tab1 () {
-    return (
-        <section className='tab1'>
-          <Link to='/' className='tab1__link' >Главная</Link>
-          <Information
-            title='Что почитать?'
-          >
-            <p className='tab1__info-title'>Информация для ознакомления:</p>
+function Tab1() {
+  return (
+    <section className='tab1'>
+      <Link to='/' className='tab1__link' >Главная</Link>
+      <Information
+        title='Что почитать?'
+      >
+        <p className='tab1__info-title'>Информация для ознакомления:</p>
+        <ul className='tab1__list'>
+          <li className='tab1__list-point'>
             <p className='tab1__info'>- Клинические рекомендации</p>
-            <a 
+            <a
               className='tab1__file'
-              href={file1} 
+              href={file1}
               target="_blank"
-              rel="noreferrer"> 
+              rel="noreferrer">
               Открыть файл PDF
             </a>
+          </li>
+          <li className='tab1__list-point'>
             <p className='tab1__info'>- Практические рекомендации</p>
-            <a 
+            <a
               className='tab1__file'
-              href={file2} 
+              href={file2}
               target="_blank"
-              rel="noreferrer"> 
+              rel="noreferrer">
               Открыть файл PDF
             </a>
+          </li>
+          <li className='tab1__list-point'>
             <p className='tab1__info'>- Руководство NCCN</p>
-            <a 
+            <a
               className='tab1__file'
-              href={file3} 
+              href={file3}
               target="_blank"
-              rel="noreferrer"> 
+              rel="noreferrer">
               Открыть файл PDF
             </a>
+          </li>
+          <li className='tab1__list-point'>
             <p className='tab1__info'>- ФЗ №323-ФЗ от 21.11.2011 «Об основах охраны здоровья граждан в РФ»</p>
-            <a 
+            <a
               className='tab1__file'
-              href='https://minzdrav.gov.ru/documents/7025-federalnyy-zakon-323-fz-ot-21-noyabrya-2011-g' 
+              href='https://minzdrav.gov.ru/documents/7025-federalnyy-zakon-323-fz-ot-21-noyabrya-2011-g'
               target="_blank"
-              rel="noreferrer"> 
+              rel="noreferrer">
               ФЗ №323-ФЗ
             </a>
+          </li>
+          <li className='tab1__list-point'>
             <p className='tab1__info'>- Справочник супергерои</p>
-            <a 
+            <a
               className='tab1__file'
-              href='https://onco-patients.ru/' 
+              href='https://onco-patients.ru/'
               target="_blank"
-              rel="noreferrer"> 
+              rel="noreferrer">
               Справочник
             </a>
-          </Information>
-        </section>
-    )
+          </li>
+        </ul>
+      </Information>
+    </section>
+  )
 }
 
 export default Tab1;
