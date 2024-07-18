4d21514b1d18023040197469006c4ff907a18b32
Сергей Аветисов <karolina.kovdya-onikienko@yandex.ru>
2024-07-18 08:40:20 +0300

    change names of enu

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -15,7 +15,7 @@ function Navigation () {
               <Link to="/tab3" className="navigation__link" >Обследования</Link>
             </li>
             <li className="navigation__links">
-              <Link to="/" className="navigation__link" >ХЛТ лечение</Link>
+              <Link to="/" className="navigation__link" >ХЛТ</Link>
             </li>
             <li className="navigation__links">
               <Link to="/" className="navigation__link" >Транспозиция яичников</Link>
@@ -24,7 +24,7 @@ function Navigation () {
               <Link to="/tab6" className="navigation__link" >После ХЛТ</Link>
             </li>
             <li className="navigation__links">
-              <Link to="/" className="navigation__link" >Хирургия</Link>
+              <Link to="/" className="navigation__link" >После операции</Link>
             </li>
             <li className="navigation__links">
               <Link to="/tab8" className="navigation__link" >Клиники и ФЦ</Link>
