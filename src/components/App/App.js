4afbe34f5e34734349d38f009e1807036d90bdce
Сергей Аветисов <karolina.kovdya-onikienko@yandex.ru>
2024-08-06 21:51:27 +0300

    add component Burger

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Header from '../Header/Header';
 import Navgation from '../Navigation/Navigation';
+import Burger from '../Burger/Burger';
 import Main from '../Main/Main';
 import Tab1 from '../Tab1/Tab1';
 import Tab3 from '../Tab3/Tab3';
@@ -9,10 +10,33 @@ import Tab6 from '../Tab6/Tab6';
 import Tab7 from '../Tab7/Tab7';
 import Tab8 from '../Tab8/Tab8';
 import Footer from '../Footer/Footer';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [isBurgerOpen, setIsBurgerOpen] = useState(false);
+
+  useEffect (() => {
+    const closePopupWithEsc = (e) => {
+      if (e.key = 'Escape') {
+        closeBurgerMenu()
+      }
+    };
+    window.addEventListener('keydown', closePopupWithEsc);
+    
+    return () => window.removeEventListener('keydown', closePopupWithEsc);
+  })
+
+  function handleBurgerOpen () {
+    setIsBurgerOpen(!isBurgerOpen)
+  }
+
+  function closeBurgerMenu () {
+    setIsBurgerOpen(false)
+  }
+
   return (
     <div className="App">
+      <Burger isOpen={isBurgerOpen} onClose={closeBurgerMenu} />
       <Header />
       <Navgation />
       <Routes >
