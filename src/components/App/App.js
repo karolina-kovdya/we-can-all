ad4187bf9eb1d7d3efc1ebe208c2b5db694e8901
Сергей Аветисов <karolina.kovdya-onikienko@yandex.ru>
2024-08-22 22:01:25 +0300

    add useeffect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -26,6 +26,14 @@ function App() {
   //   return () => window.removeEventListener("keydown", closePopupWithEsc);
   // });
 
+  useEffect(() => {
+    if (isBurgerOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+  })
+
   function handleBurgerOpen () {
     setIsBurgerOpen(!isBurgerOpen)
   }
