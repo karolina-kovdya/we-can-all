5cf59324ff7620adab759546ba8aba6a6b9a5a59
Сергей Аветисов <karolina.kovdya-onikienko@yandex.ru>
2024-08-23 21:49:20 +0300

    add mobile orientaton for tab6 && tab7

--- src/components/Tab6/Tab6.js
+++ src/components/Tab6/Tab6.js
@@ -15,101 +15,101 @@ function Tab6() {
                             <th className='tab6__table-cellTitle'>Наименование</th>
                             <th className='tab6__table-cellTitle'>1-2 год</th>
                             <th className='tab6__table-cellTitle'>3-5 год</th>
-                            <th className='tab6__table-cellTitle'>После 5 лет</th>
+                            <th className='tab6__table-cellTitle'>&#62; 5 лет</th>
                         </tr>
                         <tr className='tab6__table-line'>
                             <td className='tab6__tableCell'>ОАК
                                 <span className='tab6__tableCell-speq'>*</span>
                             </td>
-                            <td className='tab6__tableCell'>Каждые 3 месяца</td>
-                            <td className='tab6__tableCell'>Каждые 6 месяцев</td>
-                            <td className='tab6__tableCell'>Не реже чем 1 раз в год</td>
+                            <td className='tab6__tableCell'>Каждые 3 мес.</td>
+                            <td className='tab6__tableCell'>Каждые 6 мес.</td>
+                            <td className='tab6__tableCell'>1 раз в год</td>
 
                         </tr>
                         <tr className='tab6__table-line'>
                             <td className='tab6__tableCell'>ОАМ
                                 <span className='tab6__tableCell-speq'>*</span>
                             </td>
-                            <td className='tab6__tableCell'>Каждые 3 месяца</td>
-                            <td className='tab6__tableCell'>Каждые 6 месяцев</td>
-                            <td className='tab6__tableCell'>Не реже чем 1 раз в год</td>
+                            <td className='tab6__tableCell'>Каждые 3 мес.</td>
+                            <td className='tab6__tableCell'>Каждые 6 мес.</td>
+                            <td className='tab6__tableCell'>1 раз в год</td>
 
                         </tr>
                         <tr className='tab6__table-line'>
                             <td className='tab6__tableCell'>БАК
                                 <span className='tab6__tableCell-speq'>*</span>
                             </td>
-                            <td className='tab6__tableCell'>Каждые 3 месяца</td>
-                            <td className='tab6__tableCell'>Каждые 6 месяцев</td>
-                            <td className='tab6__tableCell'>Не реже чем 1 раз в год</td>
+                            <td className='tab6__tableCell'>Каждые 3 мес.</td>
+                            <td className='tab6__tableCell'>Каждые 6 мес.</td>
+                            <td className='tab6__tableCell'>1 раз в год</td>
 
                         </tr>
                         <tr className='tab6__table-line'>
-                            <td className='tab6__tableCell'>Онкомаркеры SCC,РЭА, <br></br>СА-19-9 (онкомаркеры <br></br>не заменяют другдруга)
+                            <td className='tab6__tableCell'>Онкомаркеры SCC,РЭА, <br></br>СА-19-9(онкомаркеры <br></br>не заменяют друг друга)
                                 <span className='tab6__tableCell-speq'>**</span>
                             </td>
-                            <td className='tab6__tableCell'>Каждые 3 месяца</td>
-                            <td className='tab6__tableCell'>Каждые 6 месяцев</td>
-                            <td className='tab6__tableCell'>Не реже чем 1 раз в год</td>
+                            <td className='tab6__tableCell'>Каждые 3 мес.</td>
+                            <td className='tab6__tableCell'>Каждые 6 мес.</td>
+                            <td className='tab6__tableCell'>1 раз в год</td>
 
                         </tr>
                         <tr className='tab6__table-line'>
                             <td className='tab6__tableCell'>Пальцевой осмотр<br></br> анального канала <br></br> и прямой кишки</td>
-                            <td className='tab6__tableCell'>Каждые 3 месяца</td>
-                            <td className='tab6__tableCell'>Каждые 6 месяцев</td>
-                            <td className='tab6__tableCell'>Не реже чем 1 раз в год</td>
+                            <td className='tab6__tableCell'>Каждые 3 мес.</td>
+                            <td className='tab6__tableCell'>Каждые 6 мес.</td>
+                            <td className='tab6__tableCell'>1 раз в год</td>
                         </tr>
                         <tr className='tab6__table-line'>
                             <td className='tab6__tableCell'>Пальпация<br></br>лимфатических узлов<br></br>+/- УЗИ</td>
-                            <td className='tab6__tableCell'>Каждые 3 месяца</td>
-                            <td className='tab6__tableCell'>Каждые 6 месяцев</td>
-                            <td className='tab6__tableCell'>Не реже чем 1 раз в год</td>
+                            <td className='tab6__tableCell'>Каждые 3 мес.</td>
+                            <td className='tab6__tableCell'>Каждые 6 мес.</td>
+                            <td className='tab6__tableCell'>1 раз в год</td>
                         </tr>
                         <tr className='tab6__table-line'>
                             <td className='tab6__tableCell'>Колоноскопия<br></br>(аноскопия, <br></br>ректроманоскопия,<br></br>ТРУзи). Вид <br></br>исследования по<br></br>усмотрению врача</td>
-                            <td className='tab6__tableCell'>Каждые 3 месяца</td>
-                            <td className='tab6__tableCell'>Каждые 6 месяцев</td>
-                            <td className='tab6__tableCell'>Не реже чем 1 раз в год</td>
+                            <td className='tab6__tableCell'>Каждые 3 мес.</td>
+                            <td className='tab6__tableCell'>Каждые 6 мес.</td>
+                            <td className='tab6__tableCell'>1 раз в год</td>
                         </tr>
                         <tr className='tab6__table-line'>
                             <td className='tab6__tableCell'>МРТ малого таза с в/в<br></br>контрастированием</td>
-                            <td className='tab6__tableCell'>Каждые 3 месяца</td>
-                            <td className='tab6__tableCell'>Каждые 6 месяцев</td>
-                            <td className='tab6__tableCell'>Не реже чем 1 раз в год</td>
+                            <td className='tab6__tableCell'>Каждые 3 мес.</td>
+                            <td className='tab6__tableCell'>Каждые 6 мес.</td>
+                            <td className='tab6__tableCell'>1 раз в год</td>
                         </tr>
                         <tr className='tab6__table-line'>
                             <td className='tab6__tableCell'>Если ПЭТ – КТ
                                 <span className='tab6__tableCell-speq'>***</span>
                             </td>
-                            <td className='tab6__tableCell'>По усмотрению<br></br>врача, но не реже чем<br></br>каждые 6 месяцев,<br></br>оптимально 3 месяца<br></br>и далее переход на 6 <br></br>месяцев</td>
-                            <td className='tab6__tableCell'>Не реже чем 1 раз в год</td>
-                            <td className='tab6__tableCell'>Не реже чем 1 раз в год</td>
+                            <td className='tab6__tableCell'>По усмотрению<br></br>врача, но не реже чем<br></br>каждые 6 мес.,<br></br>оптимально 3 мес.<br></br>и далее переход на 6 <br></br>мес.</td>
+                            <td className='tab6__tableCell'>1 раз в год</td>
+                            <td className='tab6__tableCell'>1 раз в год</td>
                         </tr>
                         <tr className='tab6__table-line'>
                             <td className='tab6__tableCell'>Если комплекс КТ<br></br>грудной клетки и<br></br>органов брюшной<br></br>полости с в/в<br></br>контрастированием
                                 <span className='tab6__tableCell-speq'>***</span>
                             </td>
-                            <td className='tab6__tableCell'>Каждые 3 месяца</td>
-                            <td className='tab6__tableCell'>Не реже чем 1 раз в год</td>
-                            <td className='tab6__tableCell'>Не реже чем 1 раз в год</td>
+                            <td className='tab6__tableCell'>Каждые 3 мес.</td>
+                            <td className='tab6__tableCell'>1 раз в год</td>
+                            <td className='tab6__tableCell'>1 раз в год</td>
                         </tr>
                         <tr className='tab6__table-line'>
                             <td className='tab6__tableCell'>Консультация врача<br></br>онколога</td>
-                            <td className='tab6__tableCell'>Каждые 3 месяца на<br></br>основании<br></br>пройденных<br></br>исследований и<br></br>обследований</td>
-                            <td className='tab6__tableCell'>Каждые 6 месяцев</td>
-                            <td className='tab6__tableCell'>Не реже чем 1 раз в год</td>
+                            <td className='tab6__tableCell'>Каждые 3 мес. на<br></br>основании<br></br>пройденных<br></br>исследований и<br></br>обследований</td>
+                            <td className='tab6__tableCell'>Каждые 6 мес.</td>
+                            <td className='tab6__tableCell'>1 раз в год</td>
                         </tr>
                         <tr className='tab6__table-line'>
-                            <td className='tab6__tableCell'>Осмотр гинеколога для<br></br>женщи</td>
-                            <td className='tab6__tableCell'>Не реже чем 1 раз в год</td>
-                            <td className='tab6__tableCell'>Не реже чем 1 раз в год</td>
-                            <td className='tab6__tableCell'>Не реже чем 1 раз в год</td>
+                            <td className='tab6__tableCell'>Осмотр гинеколога для<br></br>женщин</td>
+                            <td className='tab6__tableCell'>1 раз в год</td>
+                            <td className='tab6__tableCell'>1 раз в год</td>
+                            <td className='tab6__tableCell'>1 раз в год</td>
                         </tr>
                         <tr className='tab6__table-line'>
                             <td className='tab6__tableCell tab6__tableCell_last'>ПАП тест для женщин</td>
-                            <td className='tab6__tableCell tab6__tableCell_last'>Не реже чем 1 раз в год</td>
-                            <td className='tab6__tableCell tab6__tableCell_last'>Не реже чем 1 раз в год</td>
-                            <td className='tab6__tableCell tab6__tableCell_last'>Не реже чем 1 раз в год</td>
+                            <td className='tab6__tableCell tab6__tableCell_last'>1 раз в год</td>
+                            <td className='tab6__tableCell tab6__tableCell_last'>1 раз в год</td>
+                            <td className='tab6__tableCell tab6__tableCell_last'>1 раз в год</td>
                         </tr>
                     </tbody>
                 </table>
